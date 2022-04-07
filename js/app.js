775d4e5ffcdc4bb72e343c8f3e67d77ac843f025
1269035311 <1269035311@qq.com>
2022-04-08 00:49:53 +0800

    Site updated: 2022-04-08 00:49:52

--- js/app.js
+++ js/app.js
@@ -6,19 +6,6 @@ document.addEventListener("DOMContentLoaded", function () {
     highlightKeyWords.startFromURL();
     locationHash();
 
-const changeTitle = () => {
-  sessionStorage.setItem("domTitle", document.title);
-  document.addEventListener('visibilitychange', function () {
-    const title = sessionStorage.getItem("domTitle") || document.title;
-    const titleArr = title.split(' - ') || [];
-    if (document.visibilityState == 'hidden') {
-      document.title = titleArr.length === 2 ? titleArr[1] : titleArr[0];
-    } else {
-      document.title = title;
-    }
-  });
-}
-
     volantis.pjax.push(() => {
       VolantisApp.pjaxReload();
       VolantisFancyBox.init();
